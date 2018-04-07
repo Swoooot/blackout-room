2fa2cd517454247974b21c06fb94776065e67a4c
Surho <surho007@hotmail.com>
2018-04-07 03:30:35 +0300

    small fixes try14

--- js/stickyNav.js
+++ js/stickyNav.js
@@ -1,19 +1,19 @@
 'use strict';
 
-(function() {
-    let currentScroll = window.pageYOffset;
-    let header = document.querySelector(".intro__header");
-    let intro = document.querySelector(".intro");
+// (function() {
+//     let currentScroll = window.pageYOffset;
+//     let header = document.querySelector(".intro__header");
+//     let intro = document.querySelector(".intro");
     
 
-    document.addEventListener('scroll', function(evt) {
-        currentScroll = window.pageYOffset;
-        if(currentScroll > 300) {
-            header.classList.add("intro__header--sticky");
-            intro.style.paddingTop = header.offsetHeight + 'px';
-        } else {
-            header.classList.remove("intro__header--sticky");
-            intro.style.paddingTop = '';
-        }
-    })
-})();
+//     document.addEventListener('scroll', function(evt) {
+//         currentScroll = window.pageYOffset;
+//         if(currentScroll > 300) {
+//             header.classList.add("intro__header--sticky");
+//             intro.style.paddingTop = header.offsetHeight + 'px';
+//         } else {
+//             header.classList.remove("intro__header--sticky");
+//             intro.style.paddingTop = '';
+//         }
+//     })
+// })();
