c2956761bd0abff38b6251783699c6d8c2288e9e
Surho <surho007@hotmail.com>
2018-04-14 15:43:09 +0300

    animation test1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,9 @@
 'use strict';
 
-// let menuSwitch = document.querySelector('.intro__switch');
-// let menu = document.querySelector('.navigation');
-
-// makeCollapsable(menuSwitch, menu, null, "opened__menu");
-
-// let questions = document.querySelectorAll('.faq__question');
-// let answers = document.querySelectorAll('.faq__answer');
-
-// Array.prototype.forEach.call(questions, function(item, i) {
-//     makeCollapsable(item, answers[i], "faq__question--opened");
-// });
-
 let mainTitle = document.querySelector(".intro__title");
 
 window.onload = function() {
-    mainTitle.classList.add("intro__title--loaded");
+    setTimeout(function() {
+        mainTitle.classList.add("intro__title--loaded")
+    }, 100);
 };
