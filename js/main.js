af1d484464004f58cf3e74b105825a4e246886d4
Surho <surho007@hotmail.com>
2018-04-14 15:47:02 +0300

    animation test2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 'use strict';
 
-let mainTitle = document.querySelector(".intro__title");
-
 window.onload = function() {
-    setTimeout(function() {
-        mainTitle.classList.add("intro__title--loaded")
-    }, 100);
+    document.body.classList.add("loaded");
 };
