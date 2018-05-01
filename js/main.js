c60d4dfe73180796fb47336c48feee0b75101a13
Surho <surho007@hotmail.com>
2018-05-01 23:00:35 +0300

    scroll button + social3

--- js/main.js
+++ js/main.js
@@ -7,21 +7,21 @@ window.onload = function() {
 let scrollBtn = document.querySelector('.scroll-home');
 let scrollBtnVisible = false;
 
-document.addEventListener('scroll', () => {
-    if(document.body.classList.contains('opened__menu--body')) {
-        console.log('!!!');
-        return;
-    }
-    if(window.pageYOffset <= 100 && scrollBtnVisible) {
-        scrollBtn.style.display = 'none';
-        scrollBtnVisible = false;
-    } else {
-        if(!scrollBtnVisible) {
-            scrollBtn.style.display = 'block';
-            scrollBtnVisible = true;
-        }
-    }
-})
+// document.addEventListener('scroll', () => {
+//     if(document.body.classList.contains('opened__menu--body')) {
+//         console.log('!!!');
+//         return;
+//     }
+//     if(window.pageYOffset <= 100 && scrollBtnVisible) {
+//         scrollBtn.style.display = 'none';
+//         scrollBtnVisible = false;
+//     } else {
+//         if(!scrollBtnVisible) {
+//             scrollBtn.style.display = 'block';
+//             scrollBtnVisible = true;
+//         }
+//     }
+// })
 
 scrollBtn.addEventListener('click', () => {
     scrollIt(0, 300, 'linear');
