5df5cc290577687cd68ee0222d486f75c2194973
Surho <surho007@hotmail.com>
2018-05-01 22:52:17 +0300

    scroll button + social

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
 window.onload = function() {
-    document.body.classList.add("loaded");
+    document.body.classList.add('loaded');
 };
+
+let scrollBtn = document.querySelector('.scroll-home');
+let scrollBtnVisible = false;
+
+document.addEventListener('scroll', () => {
+    if(window.pageYOffset === 0 && scrollBtnVisible) {
+        scrollBtn.style.display = 'none';
+        scrollBtnVisible = false;
+    } else {
+        if(!scrollBtnVisible) {
+            scrollBtn.style.display = 'block';
+            scrollBtnVisible = true;
+        }
+    }
+})
+
+scrollBtn.addEventListener('click', () => {
+    scrollIt(0, 300, 'linear');
+})
+
